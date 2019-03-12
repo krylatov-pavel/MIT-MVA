6b03584e6dcebafa68ddd5e3b09f0d5dddfd5c43
pavelk <krylatov.pavel@gmail.com>
2019-03-12 15:39:16 +0300

    make configs hierarchical

--- utils/helpers.py
+++ utils/helpers.py
@@ -2,31 +2,43 @@ import json
 from bunch import Bunch
 import os
 
-def _dict_to_bunch(dictionary):
-    bunch = Bunch(dictionary)
-
-    for key in bunch.keys():
-        if isinstance(bunch[key], dict):
-            bunch[key] = _dict_to_bunch(bunch[key])
-
-    return bunch
-
-
 def get_config_from_json(name):
-    """
-    Get the config from a json file
-    :param json_file:
-    :return: config(namespace) or config(dictionary)
-    """
-    fpath = os.path.join("configs", name + ".json")
+    def merge(a, b):
+        for key in b.keys():
+            if key in a:
+                if isinstance(a[key], dict) and isinstance(b[key], dict):
+                    a[key] = merge(a[key], b[key])
+                else:
+                    a[key] = b[key]
+            else:
+                a[key] = b[key]     
+        
+        return a
 
-    # parse the configurations from the config json file provided
-    with open(fpath, 'r') as config_file:
-        config_dict = json.load(config_file)
+    def dict_to_bunch(dictionary):
+        bunch = Bunch(dictionary)
+
+        for key in bunch.keys():
+            if isinstance(bunch[key], dict):
+                bunch[key] = dict_to_bunch(bunch[key])
+
+        return bunch
+
+    config_dict = {}
+    parts = name.split(".")
+    
+    for i in range(1, len(parts) + 1):
+        fname = ".".join(parts[:i]) + ".json"
+        fpath = os.path.join("configs",  fname)
+
+        if os.path.lexists(fpath):
+            # parse the configurations from the config json file provided
+            with open(fpath, 'r') as config_file:
+                config_dict = merge(config_dict, json.load(config_file))
 
     # convert the dictionary to a namespace using bunch lib
     #config = Bunch(config_dict)
-    config = _dict_to_bunch(config_dict)
+    config = dict_to_bunch(config_dict)
 
     return config
 
