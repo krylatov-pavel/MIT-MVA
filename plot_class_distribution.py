e3aed1c23464ffcaeff97b5e283200156541e0f7
pavelk <krylatov.pavel@gmail.com>
2019-06-03 17:24:49 +0300

    plot all classes

--- plot_class_distribution.py
+++ plot_class_distribution.py
@@ -9,16 +9,17 @@ from collections import namedtuple
 from utils.dirs import create_dirs
 
 DB_NAME = "mitdb"
+PLOT_PATH = "data\\stats"
+CLASSES_DIR = "classes"
 FS = 360
 BEATS = [
-    (["L"], "Left bundle branch block beat"),
+    (["L"], "Left bundle branch block beat",),
     (["R"], "Right bundle branch block beat"),
     (["A"], "Atrial premature beat")
 ]
 RYTHMS = [
     ("(AB", "Atrial bigeminy")
 ]
-PLOT_PATH = "data\\stats"
 
 def _filter(df, rythm=None, beats=[]):
     mask = None
@@ -40,7 +41,35 @@ def _filter(df, rythm=None, beats=[]):
 def _format_sec(sec):
     return str(datetime.timedelta(seconds=sec)).split(".")[0]
 
-def _histogram(df, name, records):
+def _plot_all(all_stats):
+    create_dirs([os.path.join(PLOT_PATH, DB_NAME)])
+
+    all_stats.sort(key=lambda x: x[1], reverse=True)
+
+    classes = [s[0] for s in all_stats]
+    durations = [s[1] for s in all_stats]
+
+    x_pos = np.arange(len(classes))
+    y_pos =  np.arange(max(durations) // 300 + 1) * 300.0
+    
+    plt.figure(figsize=(20,10))
+    plt.bar(x_pos, durations)
+
+    plt.xticks(x_pos, classes, rotation=90)
+    plt.yticks(y_pos, [_format_sec(sec) for sec in y_pos])
+
+    plt.ylabel("Duration")
+    plt.xlabel("Classes")
+
+    plt.title("All classes")
+    plt.grid(axis="y")
+
+    fpath = os.path.join(PLOT_PATH, DB_NAME, "all.png")
+    plt.savefig(fpath)   
+
+def _plot_class(df, name, records):
+    create_dirs([os.path.join(PLOT_PATH, DB_NAME, CLASSES_DIR)])
+
     durations = []
     
     for rec in records:
@@ -65,7 +94,7 @@ def _histogram(df, name, records):
     plt.grid(axis="y")
 
     fname = "{}.png".format(name)
-    fpath = os.path.join(PLOT_PATH, DB_NAME, fname)
+    fpath = os.path.join(PLOT_PATH, DB_NAME, CLASSES_DIR, fname)
     plt.savefig(fpath)
 
 def _load_raw_data():
@@ -103,15 +132,19 @@ def main():
     df = _load_raw_data()
     records = df.record.unique()
     
-    create_dirs([os.path.join(PLOT_PATH, DB_NAME)])
+    all_classes = []
 
     for b in BEATS:
         beats = _filter(df, beats=b[0])
-        _histogram(beats, name=b[1], records=records)
+        _plot_class(beats, name=b[1], records=records)
+        all_classes.append((b[1], (beats.end - beats.start).sum() / FS))
     
     for r in RYTHMS:
         beats = _filter(df, rythm=r[0])
-        _histogram(beats, name=r[1], records=records)
+        _plot_class(beats, name=r[1], records=records)
+        all_classes.append((r[1], (beats.end - beats.start).sum() / FS))
+
+    _plot_all(all_classes)
 
 if __name__ == "__main__":
     main()
